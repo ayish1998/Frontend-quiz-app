1ea2bd1659ed757bd82055a2e373b0042089b43a
Ayishatu Saeed <91335764+ayish1998@users.noreply.github.com>
2024-06-03 21:27:51 +0000

    Modify the Quiz component to display the images well

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,15 @@
 // Code for the Quiz component
 import { useState } from 'react';
-import iconMoonDark from '/src/assets/images/icon-moon-dark.svg';
-import iconMoonLight from '/src/assets/images/icon-moon-light.svg';
-import iconSunDark from '/src/assets/images/icon-sun-dark.svg';
-import iconSunLight from '/src/assets/images/icon-sun-light.svg';
-import backgroundDesktopDark from '/src/assets/images/pattern-background-desktop-dark.svg';
-import backgroundDesktopLight from '/src/assets/images/pattern-background-desktop-light.svg';
-import backgroundMobileDark from '/src/assets/images/pattern-background-mobile-dark.svg';
-import backgroundMobileLight from '/src/assets/images/pattern-background-mobile-light.svg';
-import backgroundTabletDark from '/src/assets/images/pattern-background-tablet-dark.svg';
-import backgroundTabletLight from '/src/assets/images/pattern-background-tablet-light.svg';
+import iconMoonDark from './src/assets/images/icon-moon-dark.svg';
+import iconMoonLight from './src/assets/images/icon-moon-light.svg';
+import iconSunDark from './src/assets/images/icon-sun-dark.svg';
+import iconSunLight from './src/assets/images/icon-sun-light.svg';
+import backgroundDesktopDark from './src/assets/images/pattern-background-desktop-dark.svg';
+import backgroundDesktopLight from './src/assets/images/pattern-background-desktop-light.svg';
+import backgroundMobileDark from './src/assets/images/pattern-background-mobile-dark.svg';
+import backgroundMobileLight from './src/assets/images/pattern-background-mobile-light.svg';
+import backgroundTabletDark from './src/assets/images/pattern-background-tablet-dark.svg';
+import backgroundTabletLight from './src/assets/images/pattern-background-tablet-light.svg';
 
 function Quiz() {
   // State for toggling dark mode
@@ -22,10 +22,10 @@ function Quiz() {
 
   console.log("Rendering Quiz component"); 
   const subjects = [
-    { name: 'HTML', icon: '/src/assets/images/icon-html.svg' },
-    { name: 'CSS', icon: '/src/assets/images/icon-css.svg' },
-    { name: 'JavaScript', icon: '/src/assets/images/icon-js.svg' },
-    { name: 'Accessibility', icon: '/src/assets/images/icon-accessibility.svg' },
+    { name: 'HTML', icon: './src/assets/images/icon-html.svg' },
+    { name: 'CSS', icon: './src/assets/images/icon-css.svg' },
+    { name: 'JavaScript', icon: './src/assets/images/icon-js.svg' },
+    { name: 'Accessibility', icon: './src/assets/images/icon-accessibility.svg' },
   ];
 
   const handleSubjectClick = (subject) => {
